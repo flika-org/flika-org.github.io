930e32a38781a3d1d563da288a4e01456b472c68
Brett Settle <brettjsettle@gmail.com>
2016-02-04 16:20:52 -0800

    moved js out of documentatino

--- static/js/main.js
+++ static/js/main.js
@@ -25,6 +25,35 @@ jQuery(function($) {'use strict';
 		})
 	});
 
+    $(document).ready(function() {
+        function isScrolledTo(elem) {
+            var docViewTop = $(window).scrollTop(); //num of pixels hidden above current screen
+            var docViewBottom = docViewTop + $(window).height();
+ 
+            var elemTop = $(elem).offset().top; //num of pixels above the elem
+            var elemBottom = elemTop + $(elem).height();
+ 
+            return ((elemTop <= docViewTop));
+        }
+ 
+        var catcher = $('#catcher');
+        var sticky = $('#sticky');
+        sticky.css('position','fixed');
+        sticky.css('position','absolute'); //quick fix
+
+        $(window).scroll(function() {
+            if(isScrolledTo(sticky)) {
+                sticky.css('position','fixed');
+                sticky.css('top','0px');
+            }
+            var stopHeight = catcher.offset().top + catcher.height();
+            if ( stopHeight > sticky.offset().top) {
+                sticky.css('position','absolute');
+                sticky.css('top',stopHeight);
+            }
+        });
+    });
+
 	//Initiat WOW JS
 	new WOW().init();
 
